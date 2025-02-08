4ebad49330615e13ee63d4ec1f8aec6be8d8b40e
shivamprakash <shivamprakash@192.168.20.157>
2025-02-09 03:01:18 +0530

    push2

--- SystemDesignCodes/src/com/undopattern/Texteditor.java
+++ SystemDesignCodes/src/com/undopattern/Texteditor.java
@@ -3,7 +3,9 @@ package com.undopattern;
 public class Texteditor {
   
    private String content;
+   
    public void write() {
 	   System.out.println("writeen");
+	   System.out.println("fast");
    }
 }
